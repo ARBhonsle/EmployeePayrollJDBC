ae0cb827e92e77b8d98e9464078427615d7b9efd
Aditi Bhonle <aditirbhonsle2824@gmail.com>
2021-09-20 19:53:52 +0530

    Added: Connection to database

--- src/main/java/EmployeePayrollJDBC.java
+++ src/main/java/EmployeePayrollJDBC.java
@@ -1,3 +1,25 @@
-public class EmployeePayrollJDBC {
+import java.sql.*;
 
+public class EmployeePayrollJDBC {
+    static Connection connection;
+    static Statement statement;
+
+
+
+    public void executeGivenQuery(String query) throws SQLException {
+        ResultSet resultSet = statement.executeQuery(query);
+        ResultSetMetaData resultSetMetaData = resultSet.getMetaData();
+        for(int i=1;i<=resultSetMetaData.getColumnCount();i++){
+            System.out.println(resultSetMetaData.getColumnLabel(i));
+        }
+    }
+
+    public EmployeePayrollJDBC(String url, String user, String password) {
+        try {
+            connection = DriverManager.getConnection(url, user, password);
+            statement = connection.createStatement();
+        } catch (SQLException sqlException) {
+            sqlException.printStackTrace();
+        }
+    }
 }
--- src/test/java/EmployeePayrollTest.java
+++ src/test/java/EmployeePayrollTest.java
@@ -1,2 +1,15 @@
+import org.junit.jupiter.api.Test;
+
+import java.sql.SQLException;
+
 public class EmployeePayrollTest {
+    @Test
+    public void givenUrlUserPasswordQuery_checksConnectionToDatabase_returnsQuery() throws SQLException {
+        String url = "jdbc:mysql://localhost:3306/payroll_service";
+        String user="root";
+        String password="root@1234";
+        String query="select * from employee_payroll;";
+        EmployeePayrollJDBC payroll = new EmployeePayrollJDBC(url,user,password);
+        payroll.executeGivenQuery(query);
+    }
 }
